d5eecbbd5876f81bc1efb80ced41ea341bc9a894
chesterho3363 <ho3363@gmail.com>
2020-05-28 11:27:44 +0800

    1st commit

---
+++ 1.py
@@ -0,0 +1,8 @@
+=== SQL Server 2012 Express ===
+1. Start "SQL Server (SQLEXPRESS)" Service
+
+=== SQL Server Management Studio Express ===
+1. Authentication Method: SQL Server Authentication
+2. Server Name:(local)\SQLEXPRESS
+3. Login Account: sa
+4. Login Password: abc123
